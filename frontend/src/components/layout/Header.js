2840ae0412a039e38dd2799d58ca81ad9a4a6b69
akhil79 <akhilpaulweb@gmail.com>
2021-12-21 18:04:23 +0600

    frontend panel set up.

---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import {Typography,Toolbar} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+const drawerWidth = 240;
+function Header(props){
+    return(
+      <AppBar
+      position="fixed"
+      sx={{
+        width: { sm: `calc(100% - ${drawerWidth}px)` },
+        ml: { sm: `${drawerWidth}px` },
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+         // onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          Admin Dashboard
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    )
+}
+
+export default Header;
---
+++ frontend/src/components/layout/SidebarData.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Dashboard from '@mui/icons-material/Dashboard';
+import AddBox from '@mui/icons-material/AddBox';
+import AppsIcon from '@mui/icons-material/Apps';
+import SettingsIcon from '@mui/icons-material/Settings';
+import CategoryIcon from '@mui/icons-material/Category';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'; 
+
+
+export const SidebarDatas = [
+    {
+        title: "Dashboard",
+        icon :<Dashboard/> ,
+        Link : "/"
+    },
+    {
+        title: "Add Product",
+        icon :<AddBox/> ,
+        Link : "/admin/addproduct" 
+    },
+    {
+        title: "Manage Product",
+        icon :<AppsIcon/> ,
+        Link : "/admin/manageproduct"
+    },
+    {
+        title: "Add category",
+        icon :<AddBox/> ,
+        Link : "/admin/addcategory"    
+    },
+    {
+        title: "Manage Category",
+        icon :<CategoryIcon/> ,
+        Link : "/admin/managecat"    
+    },
+    {
+        title: "Settings",
+        icon :<SettingsIcon/>,
+        Link:"/admin/setting",
+        ExpandMoreIcon:<ExpandMoreIcon/>,
+        ExpandLessIcon:<ExpandLessIcon/>,
+        subnav:[
+            {
+                title:"Categories",
+                Link:"/admin/categories"
+            },
+            {
+                title:"add category",
+                Link:"/admin/addcategory"
+            },
+        ]
+    
+        },
+]
