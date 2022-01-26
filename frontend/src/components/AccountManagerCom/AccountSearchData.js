437bbf00e93269b19705353ceedb2a5d2e58d15a
akhil79 <akhilpaulweb@gmail.com>
2022-01-26 11:56:47 +0600

    Some change in frontend.

--- frontend/src/components/AccountManagerCom/AccountSearchData.js
+++ frontend/src/components/AccountManagerCom/AccountSearchData.js
@@ -1,5 +1,11 @@
 import React, { useEffect } from 'react';
 import { Grid, Typography, Paper } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import { makeStyles } from '@mui/styles';
 import Spinner from '../Spinner';
 import moment from 'moment';
@@ -34,7 +40,7 @@ function AccountSearchData() {
     const dispatch = useDispatch();
     const accountHolder = useSelector((state) => state.allAccountHoders.accountHolder);
     const loading = useSelector((state) => state.allAccountHoders.loading);
-    
+
     useEffect(() => {
         dispatch(singleAccount(params.accountNo))
     }, [params.accountNo]);
@@ -43,54 +49,85 @@ function AccountSearchData() {
             {
                 loading ?
                     <Spinner /> :
-                    <div>         
-                {
-                accountHolder.map((ac)=>(
-                <div>
-                    <Grid className={classes.AcInfo} item xs={12} sm={12} md={12}>
-                        <div>
-                            <Typography variant="h3">{ac.FullName}</Typography>
-                            <Typography>Account No : {ac.AccountNo}</Typography>
-                            <Typography>Account Type : {ac.AccountType}</Typography>
-                            <Typography>Mobile : {ac.Mobile}</Typography>
-                            <Typography>Email : {ac.Email}</Typography>
-                            <Typography>Address : {ac.Address}</Typography>
-                            <Typography>Birth Date : {ac.Birthdate}</Typography>
-                            <Typography>Nominee : {ac.Nominee}</Typography>
-                            <Typography>Total Balance : {ac.MainBalance}</Typography>
-                        </div>
-                        <Paper>
-                            <div style={{display:"inline-flex",width:"170px",height:"210px"}}>
-                                <img src={"http://localhost:3001/imagereader/"+ac.Picture}/>
-                            </div>
-                        </Paper>
-                    </Grid>
-                    <Grid>
-                        <Grid className={classes.tranOperation} item xs={12} sm={12} md={12}>
-                            <Paper>
-                                <Typography variant="h6">Signature Verification :</Typography>
-                                <div style={{display:"inline-flex",width:"240px",height:"200px"}}>
-                                    <img src={"http://localhost:3001/imagereader/"+ac.Signature} />
-                                </div>
-                            </Paper>
-                            <Paper>
-                                <Typography variant="h5">All Transactions</Typography>
-                                {ac.Transaction.map((tran)=>(
+                    <div>
+                        {
+                            accountHolder.map((ac) => (
                                 <div>
-                                    <Typography>
-                                      {moment(tran.Date).format('DD-MM-YYYY')} 
-                                    - {tran.TransactionType} 
-                                    - {tran.Deposit}{tran.Withdraw}{tran.Transfer} 
-                                    -{tran.Balance}
-                                    </Typography>
+                                    <Grid className={classes.AcInfo} item xs={12} sm={12} md={12}>
+                                        <div>
+                                            <Typography variant="h3">{ac.FullName}</Typography>
+                                            <Typography>Account No : {ac.AccountNo}</Typography>
+                                            <Typography>Account Type : {ac.AccountType}</Typography>
+                                            <Typography>Mobile : {ac.Mobile}</Typography>
+                                            <Typography>Email : {ac.Email}</Typography>
+                                            <Typography>Address : {ac.Address}</Typography>
+                                            <Typography>Birth Date : {ac.Birthdate}</Typography>
+                                            <Typography>Nominee : {ac.Nominee}</Typography>
+                                            <Typography>Total Balance : {ac.MainBalance}</Typography>
+                                        </div>
+                                        <Paper>
+                                            <div style={{ display: "inline-flex", width: "170px", height: "210px" }}>
+                                                <img src={"http://localhost:3001/imagereader/" + ac.Picture} />
+                                            </div>
+                                        </Paper>
+                                    </Grid>
+                                    <Grid>
+                                        <Grid className={classes.tranOperation} item xs={12} sm={12} md={12}>
+                                            <Paper>
+                                                <Typography variant="h6">Signature Verification :</Typography>
+                                                <div style={{ display: "inline-flex", width: "240px", height: "200px" }}>
+                                                    <img src={"http://localhost:3001/imagereader/" + ac.Signature} />
+                                                </div>
+                                            </Paper>
+                                            <Paper>
+                                                <TableContainer component={Paper}>
+                                                    <Table sx={{ minWidth: 585 }} aria-label="spanning table">
+                                                        <TableHead>
+                                                            <TableRow>
+                                                                <TableCell align="center" colSpan={3}>
+                                                                    All Transactions
+                                                                </TableCell>
+                                                                <TableCell align="right">After Transaction</TableCell>
+                                                            </TableRow>
+                                                            <TableRow>
+                                                                <TableCell>Date</TableCell>
+                                                                <TableCell align="left">Type</TableCell>
+                                                                <TableCell align="right">Amount</TableCell>
+                                                                <TableCell align="right">Balance</TableCell>
+                                                            </TableRow>
+                                                        </TableHead>
+                                                        <TableBody>
+                                                            {ac.Transaction.map((tran) => (
+                                                                <TableRow>
+                                                                    <TableCell>{moment(tran.Date).format('DD-MM-YYYY')}</TableCell>
+                                                                    <TableCell align="left">{tran.TransactionType}</TableCell>
+                                                                    <TableCell align="right">{tran.Deposit||tran.Withdraw||tran.Transfer}</TableCell>
+                                                                    <TableCell align="right">{tran.Balance}</TableCell>
+                                                                </TableRow>
+                                                            ))}
+                                                            {/* <TableRow>  // SubTable for Summary
+                                                                <TableCell rowSpan={3} />
+                                                                <TableCell colSpan={2}>Subtotal</TableCell>
+                                                                <TableCell align="right">6100</TableCell>
+                                                            </TableRow>
+                                                            <TableRow>
+                                                                <TableCell>Tax</TableCell>
+                                                                <TableCell align="right">7%</TableCell>
+                                                                <TableCell align="right">42.6</TableCell>
+                                                            </TableRow>
+                                                            <TableRow>
+                                                                <TableCell colSpan={2}>Total</TableCell>
+                                                                <TableCell align="right">100000</TableCell>
+                                                            </TableRow> */}
+                                                        </TableBody>
+                                                    </Table>
+                                                </TableContainer>
+                                            </Paper>
+                                        </Grid>
+                                    </Grid>
                                 </div>
-                                ))}
-                            </Paper>
-                        </Grid>
-                    </Grid>
-                </div>
-                ))
-                }      
+                            ))
+                        }
                     </div>
             }
         </Grid>
