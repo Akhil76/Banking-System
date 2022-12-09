8bade3f35fac9b1e532ef816895a6f8afa60873b
akhil76 <akhilweb@gmail.com>
2022-12-09 17:41:06 +0600

    Test

--- frontend/src/components/AccountHolders.js
+++ frontend/src/components/AccountHolders.js
@@ -41,7 +41,7 @@ function AccountHolders() {
 
     useEffect(() => {
         dispatch(allAccountHolders());
-    }, [dispatch,accountHolders]);
+    }, [dispatch]);
 
     return (
         <Grid item xs={12} sm={12} md={12}>
